561ba9e753a70de9d33a7dd0b89564df653cd013
Sevenrt <62969131+weliton209@users.noreply.github.com>
2026-03-16 00:21:16 -0300

    Update redteam.py

--- bots/redteam.py
+++ bots/redteam.py
@@ -4,11 +4,25 @@ import os
 TOKEN = os.getenv("RED_TOKEN")
 CHAT = os.getenv("RED_CHAT")
 
-msg = "🔥 Red Team Update\nNova ferramenta ofensiva detectada no GitHub"
+url = "https://api.github.com/search/repositories?q=pentesting+tool&sort=updated"
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+data = requests.get(url).json()
 
-requests.post(url,data={
-    "chat_id": CHAT,
-    "text": msg
-})
+for repo in data["items"][:5]:
+
+    name = repo["name"]
+    link = repo["html_url"]
+    desc = repo["description"]
+
+    msg = f"""
+🔥 Red Team Tool
+
+Name: {name}
+Desc: {desc}
+Repo: {link}
+"""
+
+    requests.post(
+        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        data={"chat_id": CHAT, "text": msg}
+    )
