ba221bf1532791c0570ac0ba1c8b0d48773487ba
Sevenrt <62969131+weliton209@users.noreply.github.com>
2026-03-23 19:53:41 -0300

    Update threatintel.py

--- bots/threatintel.py
+++ bots/threatintel.py
@@ -8,12 +8,17 @@ import requests
 from modules.intel.news import get_news
 from modules.intel.apt import get_apt_campaigns
 from modules.intel.ioc import get_iocs
+from modules.intel.malware import get_c2
+from modules.intel.leaks import get_leaks
+from modules.cve_exploit import get_exploitable_cves
+
 from modules.core.history import load_history, save_history, gen_id
 
 TOKEN = os.getenv("INTEL_TOKEN")
 CHAT = os.getenv("INTEL_CHAT")
 
 url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
 
 def send(msg):
     try:
@@ -24,7 +29,26 @@ def send(msg):
 
 history = load_history()
 
-send("🧠 Threat Intel Briefing ON")
+send("🧠 Threat Intel PRO ON")
+
+
+# -------------------
+# 🚨 CVE (PRIORIDADE)
+# -------------------
+for c in get_exploitable_cves():
+
+    uid = gen_id(c["id"])
+
+    if uid in history:
+        continue
+
+    send(f"""🚨 Exploitable CVE
+
+{c['id']}
+{c['desc']}
+""")
+
+    save_history(uid)
 
 
 # -------------------
@@ -41,6 +65,44 @@ for a in get_apt_campaigns():
 
 {a['title']}
 {a['link']}
+""")
+
+    save_history(uid)
+
+
+# -------------------
+# 🦠 MALWARE C2
+# -------------------
+for m in get_c2():
+
+    uid = gen_id(m["hash"])
+
+    if uid in history:
+        continue
+
+    send(f"""🦠 Malware C2
+
+Family: {m['family']}
+Hash: {m['hash']}
+""")
+
+    save_history(uid)
+
+
+# -------------------
+# 🔓 DATA BREACH
+# -------------------
+for l in get_leaks():
+
+    uid = gen_id(l["name"])
+
+    if uid in history:
+        continue
+
+    send(f"""🔓 Data Breach
+
+{l['name']}
+Domain: {l['domain']}
 """)
 
     save_history(uid)
