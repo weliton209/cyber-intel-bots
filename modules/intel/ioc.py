ee5314850706806ad73c784ce9c2c8ccb3d89b3a
Sevenrt <62969131+weliton209@users.noreply.github.com>
2026-03-23 18:54:33 -0300

    Create ioc.py

---
+++ modules/intel/ioc.py
@@ -0,0 +1,30 @@
+import requests
+
+def get_iocs():
+
+    urls = [
+        "https://feodotracker.abuse.ch/downloads/ipblocklist.txt",
+        "https://raw.githubusercontent.com/stamparm/ipsum/master/ipsum.txt"
+    ]
+
+    results = []
+
+    for url in urls:
+        try:
+            data = requests.get(url, timeout=10).text.splitlines()
+
+            for line in data:
+
+                if not line or line.startswith("#"):
+                    continue
+
+                ip = line.split()[0]
+
+                results.append({
+                    "ip": ip
+                })
+
+        except:
+            continue
+
+    return results[:10]
